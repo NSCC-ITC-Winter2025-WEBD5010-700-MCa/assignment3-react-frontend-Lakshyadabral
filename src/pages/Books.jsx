bfc2f4903d862c369359ca60011ff0347daafd6b
Lakshyadabral <lakshyadabral24554@gmail.com>
2025-02-18 15:24:20 -0400

    ICA 18th feb

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,21 +1,37 @@
 import { useQuery } from "@tanstack/react-query";
-import BooksTable from "../booksTable";
+
+import BooksTable from "../components/books/booksTable";
+import { data, Outlet } from "react-router-dom";
 
 const Books = () => {
 
-const { isPending , error , data: books } = useQuery({
+const { isPending , error , data } = useQuery({
   queryKey: ['booksData'],
   queryFn: async () => {
-    console.log('Feteching Data');
     const response = await fetch('http://localhost:3000/books')
     return response.json()
-  }
+  },
+  staleTime: Infinity
 })
 
 if(isPending) return <div> Loading...</div>
 
-return <BooksTable books={books} />;
-};
+if(error) return <div> {`An error had occured: + ${error.message}`}</div>
+
+
+
+return (
+  <div> 
+    <Outlet/>
+    <h1 className="text-2xl  font-bold"> Books </h1>
+    {
+      isPending ? <div> Loading...</div> : <BooksTable books={data}/>
+    }
+
+  </div>
+);
+
+}
 
 export default Books;
 
